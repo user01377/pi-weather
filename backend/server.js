0130b68abaa2ed914df0b7eea3c0465614e75e03
Vincent Vue <vuevincent2025@gmail.com>
2025-11-08 17:38:55 -0500

    fixed server for compatibility with cors

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,28 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from 'express';
+import cors from "cors";
 import morgan from 'morgan';
 import apiRouter from './routes/nws-api.js';
 
 const server = express();
 const PORT = 8000;
 
-server.get('/', (req, res) => {
-  res.send("Weather API is LIVE");
-});
+server.use(express.json());
+if (process.env.NODE_ENV === 'development') {
+  server.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }));
+}
 
 server.use(morgan('combined')) // for realtime logging like django
-server.use('/', apiRouter);;
+
+server.get('/', (req, res) => {
+  res.send("Weather API is LIVE")
+});
+
+server.use('/', apiRouter);
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
