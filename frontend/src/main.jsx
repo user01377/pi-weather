2e0d12e36cf8930a2ed44349ac333305ddadd9f2
Vincent Vue <vuevincent2025@gmail.com>
2025-11-09 09:20:39 -0500

    moved icons to public/ for ease of use

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Background from './Background.jsx'
+// import IconTest from './testicon.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
---
+++ frontend/src/testicon.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'
+
+export default function IconTest() {
+  return (
+    <div>
+      <img src="./public/day.svg" alt="Day icon" />
+      <img src="./public/team.svg" alt="Night icon" />
+      <img src="./public/rain.svg" alt="Rain icon" />
+      <img src="./public/snow.svg" alt="Snow icon" />
+      <img src="./public/thunder.svg" alt="Thunder icon" />
+      <img src="./public/fog.svg" alt="Fog icon" />
+      <img src="./public/cloud.svg" alt="Cloud icon" />
+    </div>
+  );
+}
