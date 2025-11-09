b7789ccc01143b5a66c90c6a6893070fa0b3704c
Vincent Vue <vuevincent2025@gmail.com>
2025-11-09 13:15:08 -0500

    styled quadrants and swapped assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,16 @@ import Background from './components/Background.jsx';
 function App() {
   const sampleData = {
     block1: {
-      item1: "Temperature: 72°F",
-      item2: "Humidity: 40%",
-      item3: "Wind: 5 mph",
-      item4: "Condition: Sunny",
+      item1: "5 mp/h (dir)",
+      item2: "56°",
+      item3: "99%",
+      item4: "56%",
     },
     block2: {
-      item1: "Sunrise: 6:30 AM",
-      item2: "Sunset: 7:45 PM",
-      item3: "UV Index: 5",
-      item4: "Visibility: 10 miles",
+      item1: "29.92 inHg",
+      item2: "7:45 PM",
+      item3: "10 NM",
+      item4: "56%",
     },
   };
 
--- frontend/src/components/WeatherDiv.jsx
+++ frontend/src/components/WeatherDiv.jsx
@@ -10,24 +10,44 @@ export default function WeatherDiv({ data }) {
         <div className="weather-block">
           <div className="header">AIR CONDITIONS</div>
 
-          <div className="quadrant">
-            <div className="quad-header">Wind</div>
-            <div className="quad-value">{data.block1.item1}</div>
+          <div className="quadrant top-left">
+            <div className="quad-content">
+              <img src="./public/display/wind.svg" alt="Wind Icon" className="quad-icon" />
+              <div className="quad-text">
+                <div className="quad-header">Wind</div>
+                <div className="quad-value">{data.block1.item1}</div>
+              </div>
+            </div>
           </div>
 
-          <div className="quadrant">
-            <div className="quad-header">Feels Like</div>
-            <div className="quad-value">{data.block1.item2}</div>
+          <div className="quadrant top-right">
+            <div className="quad-content">
+              <img src="./public/display/feelslike.svg" alt="Feels Like Icon" className="quad-icon" />
+              <div className="quad-text">
+                <div className="quad-header">Feels Like</div>
+                <div className="quad-value">{data.block1.item2}</div>
+              </div>
+            </div>
           </div>
 
-          <div className="quadrant">
-            <div className="quad-header">Humidity</div>
-            <div className="quad-value">{data.block1.item3}</div>
+          <div className="quadrant bottom-left">
+            <div className="quad-content">
+              <img src="./public/display/humidity.svg" alt="Humidity Icon" className="quad-icon" />
+              <div className="quad-text">
+                <div className="quad-header">Humidity</div>
+                <div className="quad-value">{data.block1.item3}</div>
+              </div>
+            </div>
           </div>
 
-          <div className="quadrant">
-            <div className="quad-header">Chance Of Precipitation</div>
-            <div className="quad-value">{data.block1.item4}</div>
+          <div className="quadrant bottom-right">
+            <div className="quad-content">
+              <img src="./public/display/precipitation.svg" alt="Precipitation Icon" className="quad-icon" />
+              <div className="quad-text">
+                <div className="quad-header">Precipitation</div>
+                <div className="quad-value">{data.block1.item4}</div>
+              </div>
+            </div>
           </div>
         </div>
 
@@ -35,25 +55,45 @@ export default function WeatherDiv({ data }) {
         <div className="weather-block">
           <div className="header">MISC INFO</div>
 
-          <div className="quadrant">
-            <div className="quad-header">Pressure</div>
-            <div className="quad-value">{data.block2.item1}</div>
+          <div className="quadrant top-left">
+            <div className="quad-content">
+              <img src="./public/display/pressure.svg" alt="Pressure Icon" className="quad-icon" />
+              <div className="quad-text">
+                <div className="quad-header">Pressure</div>
+                <div className="quad-value">{data.block2.item1}</div>
+              </div>
+            </div>
           </div>
 
-          <div className="quadrant">
-            <div className="quad-header">Sunrise & Sunset</div>
-            <div className="quad-value">{data.block2.item2}</div>
+          <div className="quadrant top-right">
+            <div className="quad-content">
+              <div className="quad-text">
+                <div className="quad-header">Sunrise & Sunset</div>
+                <div className="quad-value">{data.block2.item2}</div>
+              </div>
+            </div>
           </div>
 
-          <div className="quadrant">
-            <div className="quad-header">Visibility</div>
-            <div className="quad-value">{data.block2.item3}</div>
+          <div className="quadrant bottom-left">
+            <div className="quad-content">
+              <img src="./public/display/visibility.svg" alt="Visibility Icon" className="quad-icon" />
+              <div className="quad-text">
+                <div className="quad-header">Visibility</div>
+                <div className="quad-value">{data.block2.item3}</div>
+              </div>
+            </div>
           </div>
 
-          <div className="quadrant">
-            <div className="quad-header">Cloud Cover</div>
-            <div className="quad-value">{data.block2.item4}</div>
+          <div className="quadrant bottom-right">
+            <div className="quad-content">
+              <img src="./public/display/cloudcover.svg" alt="Cloud Cover Icon" className="quad-icon" />
+              <div className="quad-text">
+                <div className="quad-header">Cloud Cover</div>
+                <div className="quad-value">{data.block2.item4}</div>
+              </div>
+            </div>
           </div>
+
         </div>
 
       </div>
