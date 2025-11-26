53e1599e6216acf4afe5e150c79aa852600cf527
Vincent Vue <vuevincent2025@gmail.com>
2025-11-26 01:21:25 -0500

    changed chance for the zucc to a 50/50

--- frontend/src/utils/icon-mapper.jsx
+++ frontend/src/utils/icon-mapper.jsx
@@ -17,7 +17,7 @@ export function GetCustomIcon({ iconUrl, alt, className }) {
     const pickDeterministicNightIcon = () => {
       const h = hashString(lower); // 0–1 pseudo-random deterministic
       // console.log("Night hash:", h);
-      const icon = h < 0.6 ? "/moon.svg" : "/team.png";
+      const icon = h < 0.5 ? "/moon.svg" : "/team.png";
       // console.log("Chosen night icon:", icon);
       return icon;
     };
